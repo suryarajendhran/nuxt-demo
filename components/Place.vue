823ebfb0e13e6158717a14df234d76bb8232ff65
Surya Rajendhran <cielograndesurya@gmail.com>
2020-08-28 10:52:42 +0530

    Initial commit

---
+++ components/Place.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="place">
+    <img :src="location.img" width="235" height="300" />
+    <h2>{{ location.name }}</h2>
+    <p>{{ location.desc }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "place",
+  props: ["location"],
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <h1>Your Upcoming Destinations</h1>
+    <div class="location-contain">
+      <place v-for="location in locations" v-bind:key="location.name" :location="location"></place>
+    </div>
+  </div>
+</template>
+
+<script>
+import Place from "~/components/Place.vue";
+
+export default {
+  data() {
+    return {
+      locations: [
+        {
+          name: "moscow",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg",
+          desc: `Moscow is the capital and most populous city of Russia, with 13.2 million residents within the city limits and 17.8 million within the urban area. Moscow has the status of a Russian federal city.`,
+        },
+        {
+          name: "paris",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg",
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`,
+        },
+        {
+          name: "rome",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg",
+          desc: `Rome's history spans more than 2,500 years. While Roman mythology dates the founding of Rome at around 753 BC, the site has been inhabited for much longer, making it one of the oldest continuously occupied sites in Europe.`,
+        },
+        {
+          name: "france",
+          img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg",
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`,
+        },
+      ],
+    };
+  },
+  components: {
+    Place,
+  },
+};
+</script>
+
+<style>
+body {
+  width: 100vw;
+  height: 100vh;
+  font-family: "NTR", sans-serif;
+  background: #eee;
+  line-height: 1.15;
+}
+
+h1 {
+  text-align: center;
+}
+
+.location-contain {
+  display: flex;
+  justify-content: center;
+}
+
+.place {
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+  height: 370px;
+  justify-content: center;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 10px;
+}
+.place h2 {
+  margin: 0;
+  text-align: center;
+}
+.place img {
+  margin: 10px;
+  align-self: center;
+}
+</style>
+
